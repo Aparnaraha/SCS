e2a65453b9158ac069dacaab5bb44b5bbfbc6180
Aparnaraha <aparnaraha2912@gmail.com>
2024-05-03 21:58:00 +0530

    Task 9 updated

--- 12-JS Task/Task 9/script.js
+++ 12-JS Task/Task 9/script.js
@@ -1,8 +1,8 @@
 let count = document.querySelector('span');
 let txtArea = document.querySelector('textarea')
 
-// adding event lsiteners
-
+// adding event listeners
+    
 txtArea.addEventListener('input',function(){
     console.log(txtArea.value.length);
     count.textContent = txtArea.value.length
